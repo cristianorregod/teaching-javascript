2571d1af159e61bd389e8e9fbfbfade313e95b9e
brayanestiven18 <brayanestivenmartinez14@gmail.com>
2024-07-20 10:51:14 -0500

    solucion reto 1

---
+++ retos/reto-01-tipos-de-datos/solucion.js
@@ -0,0 +1,19 @@
+// Número
+let peso = 70.5;
+console.log('Valor: ', peso, ' Tipo de dato: ', typeof peso);
+
+// texto
+let marca = 'Nike';
+console.log('Valor: ', marca, ' Tipo de dato: ', typeof marca);
+
+// Valor boleano
+let tieneTallaGrande = true;
+console.log('Valor: ', tieneTallaGrande, ' Tipo de dato: ', typeof tieneTallaGrande);
+
+// Arreglo
+let tallas = ['S', 'M', 'L', 'XL'];
+console.log('Valor: ', tallas, ' Tipo de dato: ', typeof tallas);
+
+// Objeto
+let zapato = { marca: 'Nike', modelo: 'Air Force', precio: 120 };
+console.log('Valor: ', zapato, ' Tipo de dato: ', typeof zapato);
